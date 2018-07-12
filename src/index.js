ded3aa6fb3f0675adb55d516bd552b2eba156cdc
Seongjae Hwang <lotsofluck4m@gmail.com>
2018-07-13 03:15:50 +0900

    skeleton

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-// TODO
-console.log('TODO');
+// @flow
+import dotenv from 'dotenv';
+import path from 'path';
+import express from 'express';
+// import Debug from 'debug';
+
+dotenv.config(
+  { path: path.resolve(__dirname, '../.env') }
+);
+const app = express();
+
+// debug('asdf');
+const debug = require('debug')('react-from-scratch:server');
+debug('asdf');
+
+const port: number = parseInt(process.env.PORT) || 3000;
+app.set('port', port);
+
+const server = app.listen(port);
+server.on('listening', onListening);
+
+function onListening(param: void): void {
+  debug('Listening on ' + app.port);
+}
